bf3ce48ada5afafdc230259ceff34fe4cfa4dfb5
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:30 -0300

    Create ex24.py

---
+++ ex24.py
@@ -0,0 +1,5 @@
+metrosquad = float(input("Digite seu valor de metros^2 para a conversão em Acres : "))
+
+acres = metrosquad * 0.000247
+
+print("O seu valor {} em metros^2 convertido em acres é = {}".format(metrosquad,acres))
