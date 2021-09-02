8a089a67ed34b4d885c92b49a35c001c6ad59a35
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:24 -0300

    Create ex18.py

---
+++ ex18.py
@@ -0,0 +1,5 @@
+metroscub = float(input("Digite o seu valor em metros^3 para a conversão em Litros : "))
+
+litros = metroscub * 1000
+
+print("O seu valor {} dado em metros^3 convertido em litros é = {}".format(metroscub,litros))
