ae038179528b40d56a5e6d6c973a129bb96af287
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:28 -0300

    Create ex22.py

---
+++ ex22.py
@@ -0,0 +1,5 @@
+comprimento = float(input("Digite o seu valor em metros para a conversão dada em Jardas : "))
+
+jardas = comprimento/0.91
+
+print("O seu valor {} dado em metros convertido para Jardas é de : {}".format(comprimento,jardas))
