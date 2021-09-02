3309aeed71422d2dee2ce9ca75d066be6dddbc7d
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:04:48 -0300

    Create ex07.py

---
+++ ex07.py
@@ -0,0 +1,6 @@
+fahrenheit = float(input("Digite o valor em Graus Celsius para a Conversão : "))
+
+celsius = 5 * (fahrenheit-32)/9
+
+print ("O seu valor convertido de {} fahrenheit para Celsius foi de : {}".format(fahrenheit,celsius))
+
