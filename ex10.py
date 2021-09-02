c57976b814956414f0b0040094e160ffb9d1a73e
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:17 -0300

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,5 @@
+kmh = float(input("Digite um valor em KM/H para a conversão em M/s : "))
+
+metros = kmh/3.6
+
+print("O seu valor em {} quilômetros por hora convertido em metros por segundo foi de {}".format(kmh,metros))
