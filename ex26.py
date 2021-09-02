143d2276bbc436cdebe496b32eb13ef063e073d9
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:35 -0300

    Create ex26.py

---
+++ ex26.py
@@ -0,0 +1,5 @@
+metrosquad = float(input("Digite seu valor de metros^2 para a conversão em hectares : "))
+
+hectares = metrosquad * 0.0001
+
+print("O seu valor {} em metros^2 convertido em hectares é = {}".format(metrosquad,hectares))
