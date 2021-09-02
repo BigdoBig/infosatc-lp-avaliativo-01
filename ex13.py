25a2e6b175611c53f914e5821e691766e8dbbeef
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:21 -0300

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,5 @@
+quilometros = float(input("Digite o seu valor em quilômetros para convertermos em Milhas : "))
+
+milhas = quilometros/1.61
+
+print("o seu valor {} dado em quilometros, em milhas se converte em : {}".format(quilometros,milhas))
