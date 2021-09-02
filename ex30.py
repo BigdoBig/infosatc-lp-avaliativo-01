215d82f11acd39b501e0182580a89dd24b84fa0c
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:44 -0300

    Create ex30.py

---
+++ ex30.py
@@ -0,0 +1,5 @@
+valorreal = float(input("Digite o Valor em Reais Para a Conversão em Dólar : "))
+
+dolar = valorreal*5.16
+
+print("O seu valor em real de : {} Convertido em Dólares foi de : {}".format(valorreal, dolar))
