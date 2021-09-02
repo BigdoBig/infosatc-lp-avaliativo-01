a28d8e132f0a4d44c7d9eb63a585211973c46b41
Augusto Frelo Venturini <88850459+BigdoBig@users.noreply.github.com>
2021-09-02 15:05:26 -0300

    Create ex20.py

---
+++ ex20.py
@@ -0,0 +1,5 @@
+kg = float(input("Digite o seu valor em quilogramas para a conversão em libras : "))
+
+libras = kg/0.45
+
+print("O seu valor {} em quilogramas convertido para libras é = {}".format(kg,libras))
